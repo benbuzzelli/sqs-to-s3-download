fd70734a4031a02e24b0158a99588938b8d37e21
Chris Downer <62905338+cadowner90@users.noreply.github.com>
2020-07-01 09:33:45 -0600

    Initial Changes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,19 @@
-FROM adoptopenjdk/openjdk13-openj9:jdk-13.0.2_8_openj9-0.18.0-alpine-slim
+FROM ubuntu:14.04
+# Install dependencies
+RUN apt-get update && apt-get install -y \
+    software-properties-common
+RUN add-apt-repository universe
+RUN apt-get update && apt-get install -y \
+    apache2 \
+    curl \
+    git \
+    libapache2-mod-php5 \
+    php5 \
+    php5-mcrypt \
+    php5-mysql \
+    python3.4 \
+    python3-pip
+    
 COPY build/libs/gfo-processing-*-all.jar gfo-processing.jar
 EXPOSE 8080
 CMD ["java", "-Dcom.sun.management.jmxremote", "-Xmx128m", "-XX:+IdleTuningGcOnIdle", "-Xtune:virtualized", "-jar", "gfo-processing.jar"]
--- src/main/groovy/gfo/processing/ExtractJson.groovy
+++ src/main/groovy/gfo/processing/ExtractJson.groovy
@@ -8,62 +8,97 @@ import org.apache.camel.builder.RouteBuilder
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
 
 @Singleton
-class ExtractJson extends RouteBuilder {
+class ExtractJson extends RouteBuilder
+{
+    // @Value('${app.sqs.queue:"gegd-tpc"}')
+    @Value('${app.sqs.queue}')
+    String sqsQueueName
 
-    @Value('${app.sqs.gfoQueue}')
-    String gfoQueue
+    // @Value('${app.s3.bucket.to:"nga-blacksky-data-test"}
+    @Value('${app.s3.bucket.BlackSkyTo}')
+    String s3BucketBlackSkyTo
 
-    @Value('${app.s3.bucket.to}')
-    String s3BucketNameTo
+    // @Value('${app.s3.bucket.from:"kno-gv-o1"}')
+    @Value('${app.s3.bucket.BlackSkyFrom}')
+    String s3BucketBlackSkyFrom
 
-    @Value('${app.s3.bucket.from}')
-    String s3BucketNameFrom
+    // @Value('${app.s3.bucket.SkySatTo:"nga-skysat-data-test"}
+    @Value('${app.s3.bucket.SkySatTo}')
+    String s3BucketSkySatTo
 
-    @Value('${app.s3.directory.blackTempZip}')
-    String BlackSkyDirectory
+    // @Value('${app.s3.bucket.from:"nga-non-diode-o1"}')
+    @Value('${app.s3.bucket.SkySatFrom}')
+    String s3BucketSkySatFrom
 
-    @Value('${app.s3.directory.skyTempZip}')
-    String SkySatDirectory
+    // @Value('${app.s3.processed.dir:"ingest"}')
+    @Value('${app.s3.processed.dir}')
+    String processedDirectoryName
 
-    @Value('${app.s3.directory.GFOTempZip}')
-    String GFOTempDirectory
-
-    public String objectKeyName
-
-    @Override
+     @Override
     public void configure() throws Exception {
+        def data
+
         bindToRegistry('client', AmazonSQSClientBuilder.defaultClient())
 
-            from("aws-sqs://${gfoQueue}?amazonSQSClient=#client&delay=1000&maxMessagesPerPoll=5")
-                    .unmarshal().json()
-                    .process { exchange ->
+        from("aws-sqs://${sqsQueueName}?amazonSQSClient=#client&delay=1000&maxMessagesPerPoll=5")
+                .unmarshal().json()
+                .process { exchange ->
 
-                        println("--------Begin Message---------------------------")
-                        println(exchange.in.body.Message)
-                        println("--------End Message-----------------------------")
+                    def jsonSlurper = new JsonSlurper().parseText(exchange.in.body.Message)
 
-                        def jsonSlurper = new JsonSlurper().parseText(exchange.in.body.Message)
-                        def data = [
-                                bucketName: jsonSlurper.Records[0].s3.bucket.name,
-                                objectKey : jsonSlurper.Records[0].s3.object.key]
+                    data = [
+                            bucketName: jsonSlurper.Records[0].s3.bucket.name,
+                            objectKey: jsonSlurper.Records[0].s3.object.key
+                    ]
 
-                        List<String> objectKeyItems = data.objectKey.split("/")
-                        objectKeyName = objectKeyItems.last()
+                    List<String> objectKeyItems = data.objectKey.split("/")
+                    String objectKeyName = objectKeyItems.last()
 
-                        exchange.in.setHeaders(exchange.getIn().getHeaders())
-                        exchange.in.setHeader("CamelAwsS3Key", objectKeyName)
+                    String body = exchange.getIn().getBody(String.class);
+                    exchange.getOut().setBody(body)
+                    exchange.getOut().setHeaders(exchange.getIn().getHeaders())
 
-                        exchange.in.setHeader("CamelAwsS3DestinationKey", objectKeyName)
-                        exchange.in.setHeader("CamelAwsS3BucketName", s3BucketNameFrom)
-                        exchange.in.setHeader("CamelAwsS3BucketDestinationName", s3BucketNameTo)
+                    println("#" * 25 + " NEW MESSAGE COPY " + "#" * 102)
+                    println("-" * 25 + " OBJECT KEY " + "-" * 108)
+                    println("objectKey: " + data.objectKey)
+                    println("-" * 25 + " BUCKET NAME " + "-" * 107)
+                    println("BucketName: " + data.bucketName)
 
-                        println("ObjectKeyName: " + objectKeyName)
+                    // The key (file name) that will be copied from the S3_BUCKET_NAME_FROM
+                    exchange.in.setHeader("CamelAwsS3Key", "${data.objectKey}")
 
+                    //The key (file name) that will be used for the copied object
+                    //exchange.in.setHeader("CamelAwsS3DestinationKey", "${processedDirectoryName}/${objectKeyName}")
+
+                    if ((data.bucketName).toString().contentEquals("kno-gv-o1")) {
+
+                        // The bucket we are copying to
+                        //exchange.in.setHeader("CamelAwsS3BucketDestinationName", "${s3BucketBlackSkyTo}")
+
+                        println("-" * 25 + " SQS INFO " + "-" * 110)
+                        println "SQS message received.\nCopying from S3 bucket: ${s3BucketBlackSkyFrom}/${data.objectKey} " +
+                                "\nto S3 bucket: ${s3BucketBlackSkyTo}/${processedDirectoryName}/${objectKeyName}"
+
+                        ProcessBuilder pb = new ProcessBuilder( )
+                        String[] command = "aws s3 sync s3://kno-gv-o1/16928865/SXO00000/Imagery/products/bsg/BSG-101-20200615-030558-1570502-nitf-non-ortho.zip ./BSG"
+                        Process p = Runtime.getRuntime().exec(command);
+
+//                    }else if((data.bucketName).toString().contentEquals("nga-skysat-data-test")) {
+//
+//                        // The bucket we are copying to
+//                        exchange.in.setHeader("CamelAwsS3BucketDestinationName", "${s3BucketSkySatTo}")
+//
+//                        println("-"*25 + " SQS INFO " + "-"*110)
+//                        println "SQS message received.\nCopying from S3 bucket: ${s3BucketSkySatFrom}/${data.objectKey} " +
+//                                "\nCopying to S3 bucket: ${s3BucketSkySatTo}/${processedDirectoryName}/${objectKeyName}"
+//                    }
                     }
-                    .choice()
-                        .when(header("CamelAwsS3Key").endsWith("nitf-non-ortho.zip"))
-                            .to("aws-s3://${s3BucketNameTo}?useIAMCredentials=true&deleteAfterRead=false&operation=copyObject")
-                    .end()
+                }
+        //Logging information has been processed
+        .to("log:info")
+        .process { exchange ->
+               exchange.in.setHeader("Content-Type", "application/json")
+               exchange.getIn().setBody('{"bucketName": "' + data.bucketName + '", "objectKey": "' + data.objectKey + '"}')
+        }
     }
 }
-
