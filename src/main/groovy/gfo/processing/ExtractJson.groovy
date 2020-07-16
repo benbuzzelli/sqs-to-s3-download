33bdbd5f85024394c8fab3f0d901e0f55e3d28c3
melbsurfer <melbsurfer@gmail.com>
2020-07-16 15:43:43 -0400

    Modified to pull from S3 bucket

--- src/main/groovy/gfo/processing/ExtractJson.groovy
+++ src/main/groovy/gfo/processing/ExtractJson.groovy
@@ -1,104 +1,144 @@
 package gfo.processing
 
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import groovy.json.JsonSlurper
 import io.micronaut.context.annotation.Value
 import javax.inject.Singleton
 import org.apache.camel.builder.RouteBuilder
-
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
 
 @Singleton
 class ExtractJson extends RouteBuilder
 {
-    // @Value('${app.sqs.queue:"gegd-tpc"}')
+    // @Value('${app.sqs.queue:"LidarData"}')
     @Value('${app.sqs.queue}')
     String sqsQueueName
 
-    // @Value('${app.s3.bucket.to:"nga-blacksky-data-test"}
-    @Value('${app.s3.bucket.BlackSkyTo}')
-    String s3BucketBlackSkyTo
+    @Override
+    void configure() throws Exception
+    {
+        Integer counter = 0
 
-    // @Value('${app.s3.bucket.from:"kno-gv-o1"}')
-    @Value('${app.s3.bucket.BlackSkyFrom}')
-    String s3BucketBlackSkyFrom
-
-    // @Value('${app.s3.bucket.SkySatTo:"nga-skysat-data-test"}
-    @Value('${app.s3.bucket.SkySatTo}')
-    String s3BucketSkySatTo
-
-    // @Value('${app.s3.bucket.from:"nga-non-diode-o1"}')
-    @Value('${app.s3.bucket.SkySatFrom}')
-    String s3BucketSkySatFrom
-
-    // @Value('${app.s3.processed.dir:"ingest"}')
-    @Value('${app.s3.processed.dir}')
-    String processedDirectoryName
-
-     @Override
-    public void configure() throws Exception {
-        def data
+        Map<String, String> data
 
         bindToRegistry('client', AmazonSQSClientBuilder.defaultClient())
+        bindToRegistry('s3client', AmazonS3ClientBuilder.defaultClient())
 
-        from("aws-sqs://${sqsQueueName}?amazonSQSClient=#client&delay=1000&maxMessagesPerPoll=5")
-                .unmarshal().json()
-                .process { exchange ->
+        from("aws-sqs://${sqsQueueName}?amazonSQSClient=#client&delay=500&maxMessagesPerPoll=10&deleteAfterRead=true")
+            .unmarshal().json()
+            .process { exchange ->
 
-                    def jsonSlurper = new JsonSlurper().parseText(exchange.in.body.Message)
+                def jsonSqsMsg = new JsonSlurper().parseText(exchange.in.body.Message)
 
-                    data = [
-                            bucketName: jsonSlurper.Records[0].s3.bucket.name,
-                            objectKey: jsonSlurper.Records[0].s3.object.key
-                    ]
-
-                    List<String> objectKeyItems = data.objectKey.split("/")
-                    String objectKeyName = objectKeyItems.last()
-
-                    String body = exchange.getIn().getBody(String.class);
-                    exchange.getOut().setBody(body)
-                    exchange.getOut().setHeaders(exchange.getIn().getHeaders())
-
-                    println("#" * 25 + " NEW MESSAGE COPY " + "#" * 102)
-                    println("-" * 25 + " OBJECT KEY " + "-" * 108)
-                    println("objectKey: " + data.objectKey)
-                    println("-" * 25 + " BUCKET NAME " + "-" * 107)
-                    println("BucketName: " + data.bucketName)
-
-                    // The key (file name) that will be copied from the S3_BUCKET_NAME_FROM
-                    exchange.in.setHeader("CamelAwsS3Key", "${data.objectKey}")
-
-                    //The key (file name) that will be used for the copied object
-                    //exchange.in.setHeader("CamelAwsS3DestinationKey", "${processedDirectoryName}/${objectKeyName}")
-
-                    if ((data.bucketName).toString().contentEquals("kno-gv-o1")) {
-
-                        // The bucket we are copying to
-                        //exchange.in.setHeader("CamelAwsS3BucketDestinationName", "${s3BucketBlackSkyTo}")
-
-                        println("-" * 25 + " SQS INFO " + "-" * 110)
-                        println "SQS message received.\nCopying from S3 bucket: ${s3BucketBlackSkyFrom}/${data.objectKey} " +
-                                "\nto S3 bucket: ${s3BucketBlackSkyTo}/${processedDirectoryName}/${objectKeyName}"
-
-//                        ProcessBuilder pb = new ProcessBuilder( )
-//                        String[] command = "aws s3 sync s3://kno-gv-o1/16928865/SXO00000/Imagery/products/bsg/BSG-101-20200615-030558-1570502-nitf-non-ortho.zip ./BSG"
-//                        Process p = Runtime.getRuntime().exec(command);
-
-//                    }else if((data.bucketName).toString().contentEquals("nga-skysat-data-test")) {
-//
-//                        // The bucket we are copying to
-//                        exchange.in.setHeader("CamelAwsS3BucketDestinationName", "${s3BucketSkySatTo}")
-//
-//                        println("-"*25 + " SQS INFO " + "-"*110)
-//                        println "SQS message received.\nCopying from S3 bucket: ${s3BucketSkySatFrom}/${data.objectKey} " +
-//                                "\nCopying to S3 bucket: ${s3BucketSkySatTo}/${processedDirectoryName}/${objectKeyName}"
-//                    }
-                    }
+                // TODO: This is just here to count how many images we have looked at while
+                // monitoring the SQS queue
+                if (jsonSqsMsg.Records[0].eventVersion == "2.1") {
+                    counter++
+                    println "Images processed ${counter}"
                 }
-        //Logging information has been processed
-        .to("log:info")
-        .process { exchange ->
-               exchange.in.setHeader("Content-Type", "application/json")
-               exchange.getIn().setBody('{"bucketName": "' + data.bucketName + '", "objectKey": "' + data.objectKey + '"}')
-        }
+
+                data = [
+                    eventName: jsonSqsMsg.Records[0].eventName,
+                    bucketName: jsonSqsMsg.Records[0].s3.bucket.name,
+                    objectKey: jsonSqsMsg.Records[0].s3.object.key
+                ] as Map<String, String>
+
+                // Gets the file name from the object key
+                List<String> objectKeyItems = data.objectKey.split("/")
+                String objectKeyName = objectKeyItems.last()
+
+                // Gets the file extension type (zip, tif, png, txt, etc)
+                List<String> fileItems = objectKeyName.split("\\.")
+                String fileType = fileItems.last()
+
+                String satName
+
+                // TODO: Can we make this less hard coded?
+                switch(data.bucketName) {
+                    case "kno-gv-o1":
+                        satName = "blacksky"
+                        break
+                    case "nga-diode-o1":
+                        satName = "blacksky"
+                        break
+                    case "nga-non-diode-o1":
+                        satName = "skysat"
+                        break
+                    default:
+                        return
+                        break
+                }
+
+                // If it the eventName attribute is ObjectCreated:CompleteMultipartUpload then the file has been completely uploaded
+                // and is available for download.  In addition, if the file type is a .zip then begin processing it.
+                // TODO: eventName and fileType check should be configurable --> static final const at top
+                if (data.eventName == "ObjectCreated:CompleteMultipartUpload" && fileType == "zip") {
+                    println "-"*100
+                    println "Checking to see if we should download ${objectKeyName}..."
+
+                    // Check to see if it is blacksky and a 'nitf-non-ortho' file.
+                    if (satName == "blacksky" && objectKeyName.contains("nitf-non-ortho")) {
+                        println "It is a Blacksky image. The filename contains the phrase 'nitf-non-ortho'. We should download it! 👍"
+                        println "-"*100
+                        downloadSatImage(data?.bucketName, data?.objectKey, objectKeyName, satName)
+                    }
+                    // Check to see if it is sksat and a 'SkySatScene'.
+                    else if (satName == "skysat" && objectKeyName.contains("SkySatScene")){
+                        println "It is a Skysat image. The filename contains the phrase 'SkySatScene'. We should download it! 👍"
+                        println "-"*100
+                        downloadSatImage(data?.bucketName, data?.objectKey, objectKeyName, satName)
+                    } else {
+                        println "Nope, it doesn't fit our criteria. We shouldn't download it! 🚫"
+                        println "-"*100
+                    }
+
+                }
+
+            }
+            //.to("log:DEBUG?showBody=true&showHeaders=true")
+
     }
+
+    void downloadSatImage (String bucketName, String objectKey, String objectKeyName, String satName) {
+
+        println "#"*40
+        println "SQS message received. Copying from S3 bucket: ${bucketName}/${objectKey} " +
+                "to file: /maxar-${satName}-data/ingest/${objectKeyName}"
+        println "#"*40
+
+        // TODO: Find a way to do a check to see if the file exists on the bucket.
+        // Otherwise you will get an error:
+        // Jul 15, 2020 8:36:57 PM com.amazonaws.services.s3.internal.S3AbortableInputStream close
+        // WARNING: Not all bytes were read from the S3ObjectInputStream, aborting HTTP connection.
+        // This is likely an error and may result in sub-optimal behavior.
+        // Request only the bytes you need via a ranged GET or drain the input stream after use.
+        File filePath = new File("/maxar-${satName}-data/ingest/${objectKeyName}.temp")
+        //File filePath = new File("/tmp/${objectKeyName}.temp")
+
+        if (!filePath.exists()) {
+
+            InputStream inputStream = null
+            try {
+
+                def s3client = getContext().getRegistry().lookupByName("s3client")
+                inputStream = s3client.getObject(bucketName, objectKey).getObjectContent()
+
+                filePath.withOutputStream {out ->
+                    out << inputStream
+                }
+                println filePath.renameTo("/maxar-${satName}-data/ingest/${objectKeyName}")
+                //println filePath.renameTo("/tmp/${objectKeyName}")
+
+            } finally {
+                inputStream?.close()
+
+                println "-"*40
+                println "The file has been fully downloaded"
+                println "-"*40
+
+            }
+    }
+
+}
+
 }
